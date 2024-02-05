8f333f22fc4562fece16c1dacfbe2b1ac30b740e
rhman <rhmanibrahimmuhammad@gmail.com>
2024-02-05 20:04:41 +0200

    created error page message1

--- pages/home/Error.jsx
+++ pages/home/Error.jsx
@@ -1,9 +1,23 @@
+import { useLocation } from 'react-router-dom';
+import wrappers from '@components/css/Wrapper.module.css';
+import style from '@components/css/ErrorView.module.css';
+
+
 const Error = () => {
+
+    const location = useLocation();
+
     return (
-        <>
-            <h1>404</h1>
-        </>
+        <section className={ wrappers.defaultWrapper }> 
+            <div className={ style.message }>
+                <h1>
+                    <i className='material-icons' style={{fontSize:'64px'}}>error</i>
+                    <span>404</span>
+                </h1>
+                <p>The requested URL '<strong>{location.pathname}</strong>' does not exist.</p>
+            </div>
+        </section>
     )
 }
 
-export default Error;
+export default Error;1
